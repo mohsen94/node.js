422aca9c9ccf5016a07d1a48e1801c14efc15fb1
mohsen94 <49374848+mohsen94@users.noreply.github.com>
2019-05-08 11:01:13 -0700

    squareUsingFunction.js

---
+++ squareUsingFunction.js
@@ -0,0 +1,18 @@
+function squared(num) {
+  return num * num;
+}
+
+function cubed(num) {
+  return num * num * num;
+}
+
+function factorial(num) {
+  var x = num;
+  while (x > 1) {
+    num *= x-1;
+    x--;
+  }
+  return num;
+}
+
+console.log(squared(2));
